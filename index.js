605d08313052f9db606f5b67e8a6f3d332946553
Robison Pereira Machado <robisonpmachado@gmail.com>
2022-06-02 03:18:58 -0300

    ajuste nas flash messages

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const session = require('express-session');
+const flash = require('connect-flash');
 
-const app = express()
+const app = express();
+
+app.use(session({
+    secret: 'projeto-pokedex-modulo2-proj2',
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+  app.use(flash());
 
 const port = process.env.PORT || 3000;
 
@@ -47,7 +57,10 @@ const pokedex = [
 
 
 app.get("/", (req, res) => {
-    res.render("index", { pokedex });
+    message = req.flash('message');
+    if(!message.length) message = null;
+    console.log(message);
+    res.render("index", { pokedex, message});
 });
 
 app.get("/novo", (req, res) => {
@@ -58,19 +71,21 @@ app.post("/create", (req, res) => {
     const pokemon = req.body;
     pokemon.id = pokedex.length + 1;
     pokedex.push(pokemon);
+    req.flash('message', `pokémon "${pokemon.nome}" adicionado com sucesso!`);
     res.redirect("/");
 });
 
 app.get("/detalhes/:id", (req, res) => {
-    console.log("pokedex >>> ", pokedex);
     const id = +req.params.id;
-
+    message = req.flash('message');
+    if(!message.length) message = null;
+    console.log(message);
+    
     const pokemon = pokedex.find((p) => p && p.id === id);
-    res.render("detalhes", {pokemon});
+    res.render("detalhes", {pokemon, message});
 });
 
 app.get("/editar/:id", (req, res) => {
-    console.log("pokedex >>> ", pokedex);
     const id = +req.params.id;
 
     const pokemon = pokedex.find((p) => p && p.id === id);
@@ -82,14 +97,17 @@ app.post("/update/:id", (req, res) => {
     const newPokemon = req.body;
     newPokemon.id = id + 1;
     pokedex[id] = newPokemon;
+    req.flash('message', `pokémon "${newPokemon.nome}" atualizado com sucesso!`);
     res.redirect(`/detalhes/${newPokemon.id}`);
 });
 
 app.get("/delete/:id", (req, res) => {
     const id = +req.params.id - 1;
+    const pokemonParaExcluir = pokedex[id];
+    req.flash('message', `pokémon "${pokemonParaExcluir.nome}" excluído com sucesso!`); 
     delete pokedex[id];
 
-    res.redirect("/#cards");
+    res.redirect("/");
 });
 
 app.listen(port, () => console.log(`servidor rodando em http://localhost:${port}`))
